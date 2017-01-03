d12865f161d6ed52ae7b44334a52d7d1322b6991
ben bodine <bbodine10@gmail.com>
2017-01-03 12:32:20 -0500

    Chess Boi

---
+++ ChessBoi/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ChessBoi
+//
+//  Created by  on 1/3/17.
+//  Copyright © 2017 BenApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
